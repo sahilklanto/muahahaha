95389445057d0485e6decd13e34c1b86d39c6781
sahilklanto <sahil@klanto.com>
2023-09-25 20:08:03 +0530

    Fetching config from JSON rather than a hardcoding into a TS file

--- site.config.ts
+++ site.config.ts
@@ -1,65 +1,68 @@
 import { siteConfig } from './lib/site-config'
+import jsonConfig from 'site.config.json';
 
-export default siteConfig({
-  // the site's root Notion page (required)
-  rootNotionPageId: '5a99a01a5fa84d7aa687b9f52d20523c',
+export default siteConfig(jsonConfig);
 
-  // if you want to restrict pages to a single notion workspace (optional)
-  // (this should be a Notion ID; see the docs for how to extract this)
-  rootNotionSpaceId: null,
+// export default siteConfig({
+//   // the site's root Notion page (required)
+//   rootNotionPageId: '5a99a01a5fa84d7aa687b9f52d20523c',
 
-  // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+//   // if you want to restrict pages to a single notion workspace (optional)
+//   // (this should be a Notion ID; see the docs for how to extract this)
+//   rootNotionSpaceId: null,
 
-  // open graph metadata (optional)
-  description: 'Example Next.js Notion Starter Kit Site',
+//   // basic site info (required)
+//   name: 'Next.js Notion Starter Kit',
+//   domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
+//   author: 'Travis Fischer',
 
-  // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
-  // mastodon: '#', // optional mastodon profile URL, provides link verification
-  // newsletter: '#', // optional newsletter URL
-  // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
+//   // open graph metadata (optional)
+//   description: 'Example Next.js Notion Starter Kit Site',
 
-  // default notion icon and cover images for site-wide consistency (optional)
-  // page-specific values will override these site-wide defaults
-  defaultPageIcon: null,
-  defaultPageCover: null,
-  defaultPageCoverPosition: 0.5,
+//   // social usernames (optional)
+//   twitter: 'transitive_bs',
+//   github: 'transitive-bullshit',
+//   linkedin: 'fisch2',
+//   // mastodon: '#', // optional mastodon profile URL, provides link verification
+//   // newsletter: '#', // optional newsletter URL
+//   // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
 
-  // whether or not to enable support for LQIP preview images (optional)
-  isPreviewImageSupportEnabled: true,
+//   // default notion icon and cover images for site-wide consistency (optional)
+//   // page-specific values will override these site-wide defaults
+//   defaultPageIcon: null,
+//   defaultPageCover: null,
+//   defaultPageCoverPosition: 0.5,
 
-  // whether or not redis is enabled for caching generated preview images (optional)
-  // NOTE: if you enable redis, you need to set the `REDIS_HOST` and `REDIS_PASSWORD`
-  // environment variables. see the readme for more info
-  isRedisEnabled: false,
+//   // whether or not to enable support for LQIP preview images (optional)
+//   isPreviewImageSupportEnabled: true,
 
-  // map of notion page IDs to URL paths (optional)
-  // any pages defined here will override their default URL paths
-  // example:
-  //
-  // pageUrlOverrides: {
-  //   '/foo': '067dd719a912471ea9a3ac10710e7fdf',
-  //   '/bar': '0be6efce9daf42688f65c76b89f8eb27'
-  // }
-  pageUrlOverrides: null,
+//   // whether or not redis is enabled for caching generated preview images (optional)
+//   // NOTE: if you enable redis, you need to set the `REDIS_HOST` and `REDIS_PASSWORD`
+//   // environment variables. see the readme for more info
+//   isRedisEnabled: false,
 
-  // whether to use the default notion navigation style or a custom one with links to
-  // important pages
-  navigationStyle: 'default',
-  // navigationStyle: 'custom',
-  navigationLinks: [
-    {
-      title: 'About',
-      pageId: '6713b56883a640318e05923d546538d2'
-    },
-    {
-      title: 'Contact',
-      pageId: '137d4930bed548a0ad7065f91b3f4c44'
-    }
-  ]
-})
+//   // map of notion page IDs to URL paths (optional)
+//   // any pages defined here will override their default URL paths
+//   // example:
+//   //
+//   // pageUrlOverrides: {
+//   //   '/foo': '067dd719a912471ea9a3ac10710e7fdf',
+//   //   '/bar': '0be6efce9daf42688f65c76b89f8eb27'
+//   // }
+//   pageUrlOverrides: null,
+
+//   // whether to use the default notion navigation style or a custom one with links to
+//   // important pages
+//   // navigationStyle: 'default',
+//   navigationStyle: 'custom',
+//   navigationLinks: [
+//     {
+//       title: 'About',
+//       pageId: '6713b56883a640318e05923d546538d2'
+//     },
+//     {
+//       title: 'Contact',
+//       pageId: '137d4930bed548a0ad7065f91b3f4c44'
+//     }
+//   ]
+// })
